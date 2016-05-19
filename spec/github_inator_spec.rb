80554c329959a7f355dc02bfd6e23a7900415b71
nmusaelian-rally <nmusaelian@rallydev.com>
2016-05-18 21:31:26 -0600

    added tests for pagination and two param query

--- spec/github_inator_spec.rb
+++ spec/github_inator_spec.rb
@@ -1,7 +1,9 @@
 require 'github_inator'
-require 'spec_helpers/connector_runner'
+require 'time'
+#require 'spec_helpers/connector_runner'
 
 ORG_REPOS_ENDPOINT = "orgs/<org_name>/repos"
+REPO_COMMITS_ENDPOINT = "repos/<org_name>/<repo_name>/commits"
 
 describe GithubInator do
   before :each do
@@ -9,28 +11,62 @@ describe GithubInator do
   end
 
   describe "#make_request" do
-    it "return links if there is more than one page of results" do
-      organization = "RallyCommunity"
-      repos_endpoint = ORG_REPOS_ENDPOINT.sub('<org_name>', organization)
-      response = @connector.make_request(:get, repos_endpoint)
-      expect(response).to be_an_instance_of GithubInator::GithubResponse
-      expect(response.status).to eql(200)
-      expect(response.links.length).to eql(2)
-      expect(response.links.has_key?("next")).to be true
-      expect(response.links.has_key?("last")).to be true
+    context "pagination" do
+      before :each do
+        organization = "RallyCommunity"
+        @repos_endpoint = ORG_REPOS_ENDPOINT.sub('<org_name>', organization)
+      end
+      it "return links if there is more than one page of results" do
+        response = @connector.make_request(:get, @repos_endpoint)
+        expect(response).to be_an_instance_of GithubInator::GithubResponse
+        expect(response.status).to eql(200)
+        expect(response.links.length).to eql(2)
+        expect(response.links.has_key?("next")).to be true
+        expect(response.links.has_key?("last")).to be true
+      end
+      it "response includes next link equal 3 if page 2 is requested" do
+        page = {page: 2}
+        response = @connector.make_request(:get, @repos_endpoint, page)
+        expect(response.links["next"]).to eq(3)
+      end
+      it "response includes prev link equal 2 if page 3 is requested" do
+        page = {'page': 3}
+        response = @connector.make_request(:get, @repos_endpoint, page)
+        expect(response.links["prev"]).to eq(2)
+      end
     end
-    it "limit results by date and time" do
-      since1 = {since: '2015-03-01'}
-      since2 =  {since: '2015-03-24T13:44:10Z'}
-      endpoint = "repos/RallyCommunity/open-closed-defects-chart/commits"
-      response1 = @connector.make_request(:get, endpoint, since1)
-      expect(response1.status).to eql(200)
-      results1 = response1.body
-      expect(results1.length).to eql(2)
-      response2 = @connector.make_request(:get, endpoint, since2)
-      expect(response2.status).to eql(200)
-      results2 = response2.body
-      expect(results2.length).to eql(1)
+    context "time based queries" do
+      before :each do
+        organization = "RallyCommunity"
+        repo = "open-closed-defects-chart"
+        replacements = {'<org_name>' => organization, '<repo_name>' => repo}
+        @repo_commits_endpoint = REPO_COMMITS_ENDPOINT.gsub(/<\w+>/) {|match| replacements.fetch(match,match)}
+        puts @repo_commits_endpoint
+      end
+      it "limit results by date" do
+        since = {since: '2015-03-01'}
+        response = @connector.make_request(:get, @repo_commits_endpoint, since)
+        results = response.body
+        expect(results.length).to eql(2)
+      end
+      it "limit results by date and time" do
+        since =  {since: '2015-03-24T13:44:10Z'}
+        response = @connector.make_request(:get, @repo_commits_endpoint, since)
+        results = response.body
+        expect(results.length).to eql(1)
+      end
+      it "use two parameters" do
+        str1 = '2014-10-01T00:00:00Z'
+        str2 = '2015-03-04T00:00:00Z'
+        t1 = Time.parse(str1)
+        t2 = Time.parse(str2)
+        parameters =  {since: str1, until: str2}
+        response = @connector.make_request(:get, @repo_commits_endpoint, parameters)
+        results = response.body
+        expect(results.length).to eql(2)
+        expect(Time.parse(results.first["commit"]["author"]["date"])).to be_between(t1, t2)
+        expect(Time.parse(results.last["commit"]["author"]["date"])).to be_between(t1, t2)
+      end
     end
   end
 end
